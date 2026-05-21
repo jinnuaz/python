2ab657104ae6e8bda554dc8e6b35c1d6daf39ab0
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 11:11:24 -0700

    2 sum 2 testing

---
+++ Leetcode_twosum_2.py
@@ -0,0 +1,15 @@
+nums = [1,3,4,5,7,10,11]
+target = 9
+left = 0
+right = len(nums)-1 #7
+
+def solution(nums):
+    while(left < right):
+        if (nums[left] + nums[right] > target):
+            right = right -1
+        elif(nums[left] + nums[right] < target):
+            left = left +1
+        else
+            return[left,right]
+
+print(solution(nums))
