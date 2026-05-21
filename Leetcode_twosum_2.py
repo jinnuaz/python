de5e92063c755179b7080ba084e64e20b2ec4dcb
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 11:15:34 -0700

    left and right variables updated

--- Leetcode_twosum_2.py
+++ Leetcode_twosum_2.py
@@ -1,9 +1,10 @@
 nums = [1,3,4,5,7,10,11]
 target = 9
-left = 0
-right = len(nums)-1 #7
+ #7
 
 def solution(nums):
+    left = 0
+    right = len(nums)-1
     while(left < right):
         if (nums[left] + nums[right] > target):
             right = right -1
