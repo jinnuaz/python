f2f3ec2d13b26355a2389d7368937cd63d9927d9
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 23:18:58 -0700

    Search insert Position

---
+++ Leetcode/leetcode_35.py
@@ -0,0 +1,25 @@
+
+
+nums = [1,3,5,6]
+target = 5
+
+def searchInsert(nums: List[int]):
+
+    left    = 0
+    right   = len(nums)-1
+
+
+    while(left < right):
+        middle = int(left + right//2)
+        if target == nums[middle]:
+            return middle
+
+        elif target > nums[middle] :
+            left = middle + 1
+
+        else:
+            right = middle - 1
+
+    return left
+
+print (searchInsert(nums))
