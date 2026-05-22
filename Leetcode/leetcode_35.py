1a45ea5c4048f0b3826324e5b4bb4ffd89d5e04f
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 23:28:26 -0700

    Third case

--- Leetcode/leetcode_35.py
+++ Leetcode/leetcode_35.py
@@ -1,10 +1,13 @@
 
 
-# nums = [1,3,5,6]
-# target = 5
+# nums      = [1,3,5,6]
+# target    = 5
 
-nums = [1,3,5,6]
-target = 2
+# nums      = [1,3,5,6]
+# target    = 2
+
+nums    = [1,3,5,6]
+target  = 7
 
 
 def searchInsert(nums):
