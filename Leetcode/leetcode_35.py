838d4e3135dce9e9898407ab87c9a76e9058cd3a
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 23:30:48 -0700

    Testing 1st case

--- Leetcode/leetcode_35.py
+++ Leetcode/leetcode_35.py
@@ -1,10 +1,10 @@
 
 
-# nums      = [1,3,5,6]
-# target    = 5
-
 nums      = [1,3,5,6]
-target    = 2
+target    = 5
+
+# nums      = [1,3,5,6]
+# target    = 2
 
 # nums    = [1,3,5,6]
 # target  = 7
