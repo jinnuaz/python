62c9dc7408da7dd5793b3672f12aedce648c15a9
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 23:30:10 -0700

    Testing 2nd case

--- Leetcode/leetcode_35.py
+++ Leetcode/leetcode_35.py
@@ -3,11 +3,11 @@
 # nums      = [1,3,5,6]
 # target    = 5
 
-# nums      = [1,3,5,6]
-# target    = 2
+nums      = [1,3,5,6]
+target    = 2
 
-nums    = [1,3,5,6]
-target  = 7
+# nums    = [1,3,5,6]
+# target  = 7
 
 
 def searchInsert(nums):
