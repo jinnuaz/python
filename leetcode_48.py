b5b83be5ee15e770d8f2a543d0e29ea34329c377
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 21:16:34 -0700

    Second Example

--- leetcode_48.py
+++ leetcode_48.py
@@ -1,6 +1,7 @@
 from typing import List
 
-matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+# matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
 def rotate(matrix: List[List[int]]):
 
     left    = 0
