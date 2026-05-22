b437c2a819547e431f321a2d2144803c4e93566c
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-21 17:18:10 -0700

    Leetcode 27 Remove Element

---
+++ Leetcode_27.py
@@ -0,0 +1,14 @@
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+
+def solution(nums):
+    k = 0
+
+    for i in range(nums):
+        if nums[i] != val:
+            num[k] = num[i]
+            k += 1
+
+    return k
+
+print(solution(nums))
