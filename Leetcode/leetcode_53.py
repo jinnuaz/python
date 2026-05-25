d22f459d10f8a891ea3d9158538ae03a4b4e37af
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-25 14:59:02 -0700

    max subarray leetcode 53

---
+++ Leetcode/leetcode_53.py
@@ -0,0 +1,19 @@
+
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+def maxSubArray(nums):
+
+    maxSub  = nums[0]
+    currSum = 0
+
+    for n in nums:
+        if currSum < 0:
+            currSum = 0
+
+        currSum = currSum + n
+        maxSub = max(maxSub, currSum)
+        print(f"for i = {i} - maxSub = {maxSub}")
+
+    return maxSub
+
+print(maxSubArray(nums))
