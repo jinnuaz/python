d044d807ad81b2b290bfb8beaef181516d2ec63f
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-26 00:21:20 -0700

    Stock buy & sell

---
+++ Leetcode/LC_121_maxProfit.py
@@ -0,0 +1,19 @@
+
+prices = [7,1,5,3,6,4]
+def maxProfit(prices):
+
+    left        = 0  # left     = buy
+    right       = 1  # right    = sell
+    maxProfit   = 0
+
+    while (left < len(prices)):
+        # Profitable
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            maxProfit = max(maxProfit, profit)
+        else:
+            left = right
+        right = right + 1
+     return maxProfit
+
+print(maxProfit(prices))
