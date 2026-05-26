ac4247a6ac4f8a21a195ea86ea29f50d9e78858f
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-05-25 17:33:17 -0700

    Group Anagrams

---
+++ Leetcode/leetcode_49.py
@@ -0,0 +1,19 @@
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+def groupAnagrams(strs):
+
+    res = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+
+        res[tuple(count)].append(s)
+
+    return res.values()
+
+
+print(groupAnagrams(strs))
