2574075230be9fc41b8a5ffb6b2910d21d34335f
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-06 15:38:02 -0700

    Two Sum in for loop with range

--- Leetcode/leetcode1.py
+++ Leetcode/leetcode1.py
@@ -7,11 +7,11 @@ hash_Map = {}
 def TwoSum(nums, target):
     for i in range(len[nums]):
         diff = target - nums[i]
-        if diff in hashMap:
-            return hashMap[diff], i
-        hashMap[nums[i]] = i
+        if diff in hash_Map:
+            return hash_Map[diff], i
+        hash_Map[nums[i]] = i
 
-print(TwoSum())
+print(TwoSum(nums, target))
 
 
 
