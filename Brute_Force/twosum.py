755d28ad0fa4c39480624f729f041d15993a8e42
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-06 13:02:13 -0700

    type in the code ==

--- Brute_Force/twosum.py
+++ Brute_Force/twosum.py
@@ -4,7 +4,7 @@ target = 9
 def bruteforce_twosum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] = target:
+            if nums[i] + nums[j] == target:
                 return i, j, nums[i], nums[j]
 
 result = bruteforce_twosum(nums, target)
