d39cd6a9497a4ca9d1052e30f93eca0d2651baf0
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-06 13:00:32 -0700

    Leetcode two sum brute force

---
+++ Brute_Force/twosum.py
@@ -0,0 +1,11 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+def bruteforce_twosum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] = target:
+                return i, j, nums[i], nums[j]
+
+result = bruteforce_twosum(nums, target)
+print(f"sum of two sums = target are {nums[i]} and {nums[j]} and their indices are {i} and {j}")
