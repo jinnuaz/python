1e986934a3950e7be5ae6b396a313370d1a845ab
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-07 14:12:06 -0700

    Linked List

---
+++ linked_list/singlyNode.py
@@ -0,0 +1,19 @@
+class SinglyNode:
+    def __init__(self, val, next=None):
+        self.val= val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+
+Head = SinglyNode(1)
+A = SinglyNode(3)
+B = SinglyNode(4)
+C = SinglyNode(7)
+
+Head.next = A
+A.next = B
+B.next = C
+
+print(A)
