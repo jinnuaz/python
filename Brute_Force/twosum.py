db46f6e41e6fb6a5402192e7539d2a8f355cde44
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-06 19:21:05 -0700

    Two sum brute force update

--- Brute_Force/twosum.py
+++ Brute_Force/twosum.py
@@ -5,8 +5,7 @@ def bruteforce_twosum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
-                print(f"sum of two sums = target are {nums[i]} and {nums[j]} and their indices are {i} and {j}")
                 return i, j, nums[i], nums[j]
 
 result = bruteforce_twosum(nums, target)
-print(result)
+print(f"sum of two sums = target are {result[2]} and {result[3]} and their indices are {result[0]} and {result[1]}")
