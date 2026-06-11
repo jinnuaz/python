acbaf95d50e9a921a183539fa8601589fedb9995
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-10 19:13:46 -0700

    leetcode 3

--- Leetcode/3_Longest_Substring_Without_Repeating_Characters.py
+++ Leetcode/3_Longest_Substring_Without_Repeating_Characters.py
@@ -14,5 +14,5 @@ def lengthOfLongestSubstring(s: str):
         result = max(result, right - left + 1)
     return result
 
-res = lengthOfLongestSubstring("bbbbb")
+res = lengthOfLongestSubstring("pwwkew")
 print(res)
