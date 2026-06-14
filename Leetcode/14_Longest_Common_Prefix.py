39526fd146baee1de1987a44e6c986b25a040bf4
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-14 15:15:21 -0700

    Easy_second try - leetcode 14

--- Leetcode/14_Longest_Common_Prefix.py
+++ Leetcode/14_Longest_Common_Prefix.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
@@ -7,9 +7,9 @@ class Solution:
 
         for i in range(len(strs[0])):
             for s in strs:
-                if s[i] != s[0][i]:
+                if s[i] != strs[0][i]:
                     return res
-            res = res + s[0][i]
+            res = res + strs[0][i]
         return res
 
 obj = Solution()
