e062c111551ed20400b3379e69a9f9c7b667727c
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-20 16:33:30 -0700

    Plus One - leetcode 66

--- Leetcode/58_Length_of_Last_Word.py
+++ Leetcode/58_Length_of_Last_Word.py
@@ -7,8 +7,7 @@ class Solution:
         length = 0          # Pointer 2
         while s[i] == " ":
             i = i - 1
-        #while i >= 0 and s[i] != " ":
-        while s[i] != " ":
+        while i >= 0 and s[i] != " ":
             length = length + 1
             i = i - 1
         return length
---
+++ Leetcode/66_Plus_One.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        digits = digits[::-1]
+        one = 1                 # carry purpose
+        i = 0                   # index
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i = i + 1
+        return digits[::-1]
+
+obj = Solution()
+result = obj.plusOne([1,2,3])
