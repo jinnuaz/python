7949642e0d6c2d336a87e0adf2dbea4a5a27562e
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-20 14:17:27 -0700

    Length of the word - leetcode 58

---
+++ Leetcode/58_Length_of_Last_Word.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        # Defining two pointers
+
+        i = len(s) - 1      # Pointer 1
+        length = 0          # Pointer 2
+        while s[i] == " ":
+            i = i - 1
+        while i >= 0 and s[i] != " ":
+            length = length + 1
+            i = i - 1
+        return length
+
+
+obj = Solution()
+result = obj.lengthOfLastWord(" dfgdfg dfgdsfgd dfgdfgfgf  ")
