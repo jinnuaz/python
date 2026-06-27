a5b6e8812e5df438949ff04b3181634c4ccf41c3
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-06-27 10:19:01 -0700

    69. Sqrt(x)

---
+++ Leetcode/sqrt_x.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        left = 0
+        right = x
+        res = 0
+
+        while left < right:
+            mid = left + ((right  - left) // 2)
+            square = mid * mid
+            if square > x:
+                right = mid - 1
+
+            elif square < x:
+                left = mid + 1
+                res = mid
+
+            else:
+                return mid
+
+        return res
+
+obj = Solution()
+result = obj.mySqrt(25)
+print(result)
